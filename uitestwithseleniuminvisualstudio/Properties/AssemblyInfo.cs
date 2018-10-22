392f0337caf20e32ff91783d462d47034c5f3c73
Jose María Flores Zazo <contacto@jmfloreszazo.com>
2018-10-22 21:48:02 +0200

    Add project files.

---
+++ uitestwithseleniuminvisualstudio/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("uitestwithseleniuminvisualstudio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("uitestwithseleniuminvisualstudio")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("adf98fae-4659-4a78-ad5f-366d0ed5fbaa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
