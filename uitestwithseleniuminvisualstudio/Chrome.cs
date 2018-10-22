eeae052c2e71c6ec93fe0883bdc9532957438587
Jose María Flores Zazo <contacto@jmfloreszazo.com>
2018-10-22 21:52:31 +0200

    Hello World - Example with Chrome

---
+++ uitestwithseleniuminvisualstudio/Chrome.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace uitestwithseleniuminvisualstudio
+{
+    [TestClass]
+    public class Chrome
+    {
+        [TestMethod]
+        public void Shoud_Search()
+        {
+            using (ChromeDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.google.com/");
+                driver.FindElement(By.Id("lst-ib")).Clear();
+                driver.FindElement(By.Id("lst-ib")).SendKeys("Hello World! Sample");
+                driver.FindElement(By.Id("lst-ib")).SendKeys(OpenQA.Selenium.Keys.Enter);
+                driver.Quit();
+            }
+        }
+    }
+}
